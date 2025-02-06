bf21d68e3f5ffe2a2ea2a123e64f47929a636c53
Y++ <yams.tmt@gmail.com>
2025-02-06 13:45:06 +0530

    vite

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    allowedHosts: [
+      '02af30ff-68c5-4cc8-b5d5-5f20505d54f7.lovableproject.com',
+    ],
+  },
+});
